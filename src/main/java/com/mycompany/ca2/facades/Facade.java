4ddc0d90b0806e5525b44ca036f60e9f899b72a9
Staalet <staalthomas1@gmail.com>
2016-10-03 11:16:22 +0200

    Facade Class made

--- src/main/java/com/mycompany/ca2/facades/Facade.java
+++ src/main/java/com/mycompany/ca2/facades/Facade.java
@@ -5,10 +5,139 @@
  */
 package com.mycompany.ca2.facades;
 
+import static java.util.Collections.list;
+
 /**
  *
  * @author lucasmfredmark
  */
 public class Facade {
-    
+
+    public Person getPersonByPhoneNumber(int phonenumber) {
+        return phonenumber;
+    }
+
+    public Company getCompanyByPhoneNumber(int number) {
+        return number;
+    }
+
+    public Company getCompanyByCVR(int cvr) {
+        return cvr;
+    }
+
+    public list<Person> getPersonsWithGivenHobbie() {
+        return Person;
+    }
+
+    public list<Person> getAllPersonsByZipCode() {
+        return Person;
+    }
+
+    public list<Cityinfo> getAllZipCodesInCountry() {
+        return Cityinfo;
+    }
+
+    public list<PhoneNumber> getPhoneNumbers() {
+        return PhoneNumber;
+    }
+
+    public list<Company> getCompaniesWithEmployeeCount() {
+            return Company;
+    }
+
+    public void addAdress() {
+
+    }
+
+    public void deleteAdress() {
+
+    }
+
+    public void editAdress() {
+
+    }
+
+    public void addCityInfo() {
+
+    }
+
+    public void deleteCityInfo() {
+
+    }
+
+    public void editCityInfo() {
+
+    }
+
+    public void addCompany() {
+
+    }
+
+    public void deleteCompany() {
+
+    }
+
+    public void editCompany() {
+
+    }
+
+    public void addHobby() {
+
+    }
+
+    public void deleteHobby() {
+
+    }
+
+    public void editHobby() {
+
+    }
+
+    public void addHobbyPerson() {
+
+    }
+
+    public void deleteHobbyPerson() {
+
+    }
+
+    public void editHobbyPerson() {
+
+    }
+
+    public void addInfoentity() {
+
+    }
+
+    public void deleteInfoentity() {
+
+    }
+
+    public void editInfoentity() {
+
+    }
+
+    void addPerson() {
+
+    }
+
+    public void deletePerson() {
+
+    }
+
+    public void editPerson() {
+
+    }
+
+    public void addPhone() {
+    }
+
+    public void deletePhone() {
+
+    }
+
+    public void editPhone() {
+
+    }
+
 }
