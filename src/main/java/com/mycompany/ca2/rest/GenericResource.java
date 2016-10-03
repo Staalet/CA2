89dae57ffddcd9cbb46bd75d7ee84a6e22dd0742
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2016-10-03 10:22:53 +0200

    project structure

--- src/main/java/com/mycompany/ca2/rest/GenericResource.java
+++ src/main/java/com/mycompany/ca2/rest/GenericResource.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.ca2;
+package com.mycompany.ca2.rest;
 
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
