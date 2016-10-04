50586b8982c40c58bb3cf3676b0ee3eeae5e9792
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2016-10-04 15:47:40 +0200

    REST method added

--- src/main/java/com/mycompany/ca2/facades/Facade.java
+++ src/main/java/com/mycompany/ca2/facades/Facade.java
@@ -9,7 +9,9 @@ import com.mycompany.ca2.entities.Company;
 import com.mycompany.ca2.entities.Person;
 import com.mycompany.ca2.facades.interfaces.IFacade;
 import java.util.List;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -24,12 +26,30 @@ public class Facade implements IFacade {
 
     @Override
     public List<Person> getAllPersons() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            TypedQuery<Person> result = em.createNamedQuery("Person.findAll", Person.class);
+            List<Person> persons = result.getResultList();
+            
+            return persons;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public Person getPersonById(int personId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            TypedQuery<Person> result = em.createNamedQuery("Person.findByPersonId", Person.class);
+            Person p = result.setParameter("person_id", personId).getSingleResult();
+            
+            return p;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
--- src/main/java/com/mycompany/ca2/rest/PersonResource.java
+++ src/main/java/com/mycompany/ca2/rest/PersonResource.java
@@ -5,8 +5,12 @@
  */
 package com.mycompany.ca2.rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mycompany.ca2.entities.Person;
 import com.mycompany.ca2.facades.Facade;
 import com.mycompany.ca2.facades.interfaces.IFacade;
+import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
@@ -30,6 +34,7 @@ public class PersonResource {
     private UriInfo context;
     
     private final IFacade facade;
+    private final static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     /**
      * Creates a new instance of PersonResource
@@ -48,6 +53,15 @@ public class PersonResource {
     public String getJson() {
         //TODO return proper representation object
         throw new UnsupportedOperationException();
+    }
+    
+    @GET
+    @Path("complete")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllPersons() {
+        List<Person> people = facade.getAllPersons();
+        
+        return GSON.toJson(people);
     }
 
     /**
