535e90374a54dfcf54166220ae2341b15e823d2c
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2016-10-06 15:33:27 +0200

    new SQL script for tables

---
+++ scriptv2.sql
@@ -0,0 +1,157 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: localhost (MySQL 5.6.28)
+# Database: ca2
+# Generation Time: 2016-10-06 13:21:59 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table ADDRESS
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `ADDRESS`;
+
+CREATE TABLE `ADDRESS` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ADDITIONALINFO` varchar(255) DEFAULT NULL,
+  `STREET` varchar(255) DEFAULT NULL,
+  `CITYINFO_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_ADDRESS_CITYINFO_ID` (`CITYINFO_ID`),
+  CONSTRAINT `FK_ADDRESS_CITYINFO_ID` FOREIGN KEY (`CITYINFO_ID`) REFERENCES `CITYINFO` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table CITYINFO
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `CITYINFO`;
+
+CREATE TABLE `CITYINFO` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CITY` varchar(255) DEFAULT NULL,
+  `ZIPCODE` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table COMPANY
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `COMPANY`;
+
+CREATE TABLE `COMPANY` (
+  `ID` int(11) NOT NULL,
+  `NUMEMPLOYEES` varchar(255) DEFAULT NULL,
+  `CVR` varchar(255) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `MARKETVALUE` varchar(255) DEFAULT NULL,
+  `NAME` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `FK_COMPANY_ID` FOREIGN KEY (`ID`) REFERENCES `INFOENTITY` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table HOBBY
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `HOBBY`;
+
+CREATE TABLE `HOBBY` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `NAME` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table INFOENTITY
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `INFOENTITY`;
+
+CREATE TABLE `INFOENTITY` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DTYPE` varchar(31) DEFAULT NULL,
+  `EMAIL` varchar(255) DEFAULT NULL,
+  `ADDRESS_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_INFOENTITY_ADDRESS_ID` (`ADDRESS_ID`),
+  CONSTRAINT `FK_INFOENTITY_ADDRESS_ID` FOREIGN KEY (`ADDRESS_ID`) REFERENCES `ADDRESS` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table PERSON
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `PERSON`;
+
+CREATE TABLE `PERSON` (
+  `ID` int(11) NOT NULL,
+  `FIRSTNAME` varchar(255) DEFAULT NULL,
+  `LASTNAME` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `FK_PERSON_ID` FOREIGN KEY (`ID`) REFERENCES `INFOENTITY` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table PERSON_HOBBY
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `PERSON_HOBBY`;
+
+CREATE TABLE `PERSON_HOBBY` (
+  `hobbies_ID` int(11) NOT NULL,
+  `persons_ID` int(11) NOT NULL,
+  PRIMARY KEY (`hobbies_ID`,`persons_ID`),
+  KEY `FK_PERSON_HOBBY_persons_ID` (`persons_ID`),
+  CONSTRAINT `FK_PERSON_HOBBY_hobbies_ID` FOREIGN KEY (`hobbies_ID`) REFERENCES `HOBBY` (`ID`),
+  CONSTRAINT `FK_PERSON_HOBBY_persons_ID` FOREIGN KEY (`persons_ID`) REFERENCES `INFOENTITY` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table PHONE
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `PHONE`;
+
+CREATE TABLE `PHONE` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `NUMBER` int(11) DEFAULT NULL,
+  `INFOENTITY_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_PHONE_INFOENTITY_ID` (`INFOENTITY_ID`),
+  CONSTRAINT `FK_PHONE_INFOENTITY_ID` FOREIGN KEY (`INFOENTITY_ID`) REFERENCES `INFOENTITY` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
