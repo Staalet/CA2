3cc5927d7ec0aa79e63866355fd653a07623f78c
MalteBruun <MalteBruun@10.50.137.18>
2016-10-07 11:19:44 +0200

    PersonResource Integration Test

--- src/test/java/com/mycompany/ca2/rest/PersonResourceIT.java
+++ src/test/java/com/mycompany/ca2/rest/PersonResourceIT.java
@@ -5,15 +5,17 @@
  */
 package com.mycompany.ca2.rest;
 
+import com.mycompany.ca2.facades.Facade;
+import com.mycompany.ca2.facades.interfaces.IFacade;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import io.restassured.parsing.Parser;
+import javax.persistence.Persistence;
 import static org.hamcrest.Matchers.*;
 
 
@@ -26,16 +28,19 @@ public class PersonResourceIT {
     public PersonResourceIT() {
     }
     
+    private static IFacade FACADE;
+    
     @BeforeClass
     public static void setUpBeforeAll() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8080;
+        RestAssured.port = 8084;
         RestAssured.basePath = "/CA2";
         RestAssured.defaultParser = Parser.JSON;
     }
 
     @BeforeClass
     public static void setUpClass() {
+        FACADE = new Facade(Persistence.createEntityManagerFactory("PU_test"));
     }
     
     @AfterClass
@@ -75,48 +80,48 @@ public class PersonResourceIT {
     /**
      * Test of addPerson method, of class PersonResource.
      */
-    @Test
-    public void testAddPerson() {
-        System.out.println("addPerson");
-        String jsonPerson = "";
-        PersonResource instance = new PersonResource();
-        String expResult = "";
-        String result = instance.addPerson(jsonPerson);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testAddPerson() {
+//        System.out.println("addPerson");
+//        String jsonPerson = "";
+//        PersonResource instance = new PersonResource();
+//        String expResult = "";
+//        String result = instance.addPerson(jsonPerson);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of editPerson method, of class PersonResource.
      */
-    @Test
-    public void testEditPerson() {
-        System.out.println("editPerson");
-        int id = 0;
-        String jsonPerson = "";
-        PersonResource instance = new PersonResource();
-        String expResult = "";
-        String result = instance.editPerson(id, jsonPerson);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testEditPerson() {
+//        System.out.println("editPerson");
+//        int id = 0;
+//        String jsonPerson = "";
+//        PersonResource instance = new PersonResource();
+//        String expResult = "";
+//        String result = instance.editPerson(id, jsonPerson);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of deletePerson method, of class PersonResource.
      */
-    @Test
-    public void testDeletePerson() {
-        System.out.println("deletePerson");
-        int id = 0;
-        PersonResource instance = new PersonResource();
-        String expResult = "";
-        String result = instance.deletePerson(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testDeletePerson() {
+//        System.out.println("deletePerson");
+//        int id = 0;
+//        PersonResource instance = new PersonResource();
+//        String expResult = "";
+//        String result = instance.deletePerson(id);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of getAllPersons method, of class PersonResource.
@@ -133,7 +138,7 @@ public class PersonResourceIT {
      * Test of getPersonById method, of class PersonResource.
      */
     @Test
-    public void testGetPersonById() {
+    public void testGetPersonById() {        
         given().pathParam("id", 1).
         when().get("/api/person/id/{id}").
         then().
@@ -162,17 +167,17 @@ public class PersonResourceIT {
     /**
      * Test of getPersonsByHobby method, of class PersonResource.
      */
-    @Test
-    public void testGetPersonsByHobby() {
-        System.out.println("getPersonsByHobby");
-        String hobby = "";
-        PersonResource instance = new PersonResource();
-        String expResult = "";
-        String result = instance.getPersonsByHobby(hobby);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testGetPersonsByHobby() {
+//        System.out.println("getPersonsByHobby");
+//        String hobby = "";
+//        PersonResource instance = new PersonResource();
+//        String expResult = "";
+//        String result = instance.getPersonsByHobby(hobby);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of putJson method, of class PersonResource.
