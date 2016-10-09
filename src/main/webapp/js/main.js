3bf34f26795ab20abd5b4d74b1494361069d9659
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2016-10-09 16:28:09 +0200

    stuff

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,5 +1,7 @@
 $(function() {
+    /*
     var app = {
+        self: this,
         api: {
             url: 'api/',
             call: function(method, url, data, callback) {
@@ -19,18 +21,19 @@ $(function() {
             
         },
         bindEvents: function() {
-            $('#add_person_form').submit(this.events.person.add);
-            $('#edit_person_form').submit(this.events.person.edit);
-            $('#add_company_form').submit(this.events.company.add);
-            $('#edit_company_form').submit(this.events.company.edit);
+            $('form#addPerson').submit(this.handleEvents.person.add);
+            $('form#editPerson').submit(this.handleEvents.person.edit);
+            $('form#addCompany').submit(this.handleEvents.company.add);
+            $('form#editCompany').submit(this.handleEvents.company.edit);
             
-            $('.edit_person').click(this.events.company.edit);
-            $('.delete_person').click(this.events.person.remove());
+            $('.edit_person').click(this.handleEvents.company.edit);
+            $('.delete_person').click(this.handleEvents.person.remove);
         },
-        events: {
+        handleEvents: {
             person: {
                 add: function() {
-                    
+                    var data = 
+                    this.api.call('PUT', 'person/add', 1);
                 },
                 edit: function() {
                     
@@ -51,21 +54,21 @@ $(function() {
                 }
             }
         },
-        callApi: function(method, url, data) {
-            
-        },
-        forms: {
-            submit: function() {
-                
-            },
+        serializeForm: function(form) {
+            var formData = $(form).serializeArray();
+            var data = {};
+            return $(formData).each(function(i, obj) {
+                data[obj.name] = obj.value;
+            });
         }
     };
     
     app.init();
+    */
     
     var getAllPersons = function() {
         $.getJSON('api/person/all', function(persons) {
-            $('#person-table').find('tr:gt(0)').remove(); // clear existing persons from table before appending
+            $('#personTable').find('tr:gt(0)').remove(); // clear existing persons from table before appending
             $.each(persons, function(i, person) {
                 addPersonToTable(person);
             });
@@ -74,39 +77,35 @@ $(function() {
     
     var getAllCompanies = function() {
         $.getJSON('api/company/all', function(companies) {
-            $('#company-table').find('tr:gt(0)').remove(); // clear existing companies from table before appending
+            $('#companyTable').find('tr:gt(0)').remove(); // clear existing companies from table before appending
             $.each(companies, function(i, company) {
                 addCompanyToTable(company);
             });
         });
     };
     
+    getAllPersons();
+    getAllCompanies();
+    
     var addPersonToTable = function(person) {
-        var hobbiesArray = [];
-        $.each(person.hobbies, function(i, hobby) {
-            hobbiesArray.push(hobby.name);
-        });
-        var hobbies = (hobbiesArray.length > 0 ? hobbies.join() : 'No hobbies'); // if the person has hobbies, concatenate by comma
-        
-        $('#person-table').append(
+        $('#personTable').append(
             '<tr>' +
             '<td>' + person.firstName + '</td>' +
             '<td>' + person.lastName + '</td>' +
-            '<td>' + hobbies + '</td>' +
-            '<td class="text-right"><div class="edit_person glyphicon glyphicon-pencil"></div> <div class="remove_person glyphicon glyphicon-remove"></div></td>' +
+            '<td class="text-right"><div class="editPersonBtn glyphicon glyphicon-pencil"></div> <div class="deletePersonBtn glyphicon glyphicon-remove"></div></td>' +
             '</tr>'
         );
     };
     
     var addCompanyToTable = function(company) {
-        $('#company-table').append(
+        $('#companyTable').append(
             '<tr>' +
             '<td>' + company.name + '</td>' +
             '<td>' + company.description + '</td>' +
             '<td>' + company.cvr + '</td>' +
             '<td>' + company.numEmployees + '</td>' +
             '<td>' + company.marketValue + '</td>' +
-            '<td class="text-right"><div class="edit_company glyphicon glyphicon-pencil"></div> <div class="remove_company glyphicon glyphicon-remove"></div></td>' +
+            '<td class="text-right"><div class="editCompanyBtn glyphicon glyphicon-pencil"></div> <div class="deleteCompanyBtn glyphicon glyphicon-remove"></div></td>' +
             '</tr>'
         );
     };
@@ -118,14 +117,20 @@ $(function() {
         var url = 'person/add';
         var formData = $(this).serializeArray();
         var data = {};
-        var callback = function() {
-            
-        };
         
         $(formData).each(function(i, obj) {
             data[obj.name] = obj.value;
         });
         
-        
+        $.ajax({
+            type: method,
+            url: 'api/' + url,
+            data: data,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            success: addPersonToTable
+        });
     });
 });
