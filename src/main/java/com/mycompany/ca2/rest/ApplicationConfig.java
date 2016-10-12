23fe286c92b6c3da44e9fb70912f66d6ecb26209
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2016-10-12 11:22:15 +0200

    wtf

--- src/main/java/com/mycompany/ca2/rest/ApplicationConfig.java
+++ src/main/java/com/mycompany/ca2/rest/ApplicationConfig.java
@@ -29,8 +29,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(com.mycompany.ca2.rest.CompanyResource.class);
-        resources.add(com.mycompany.ca2.rest.PersonResource.class);
     }
     
 }
